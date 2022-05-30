ef1dd886c98ccfdabbe951346a281f1beff28f3c
Sakshi Shrestha <sakshi@8squarei.com>
2022-05-30 16:20:07 +0545

    added changes in triangle.php file

--- triangle.php
+++ triangle.php
@@ -1,17 +1,20 @@
 <?php 
 
 $rows = 5;
-for($i = 0; $i <=$rows; $i++){
+
+for($i = 1; $i <=$rows; $i++){
     for($k = $i; $k = $rows; $k++){
-        echo "&nbsp;&nbsp";
+        print "&nbsp;&nbsp";
     }
 
     for($j = 1; $j <=(2* $i -1); $j++){
         if($j == 1 || $i == $rows || $j == (2 * $i - 1)){
-            echo "*";
-        }else{
-            echo "&nbsp;&nbsp;";
+            print "*";
+        } else {
+            print "&nbsp;&nbsp;";
         }
     }
     print "<br>";
 }
+
+?>
